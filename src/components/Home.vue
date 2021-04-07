0b2aa948d72fba01cc58dea45cfdb1d9aede7830
Chris <christophermoret@gmail.com>
2021-04-07 17:11:58 +0200

    Fixed vuetify issue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>Faite votre résérvation</h1>
+     <v-row>
+      <v-col cols="6" md="6">test</v-col>
+      <v-col cols="6">test2</v-col>
+    </v-row>
+         <v-row>
+      <v-col>test3</v-col>
+      <v-btn>click me</v-btn>
+    </v-row>
+    </div>
+
 </template>
 
 <script>
 export default {
-name: "Home"
-}
+  name: "Home",
+};
 </script>
 
 <style scoped>
-
 </style>
