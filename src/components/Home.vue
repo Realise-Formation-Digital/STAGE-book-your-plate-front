002c4076f3c97d2dd8cfd89537d9d0ec112457ec
Anton <grekov.anton96@gmail.com>
2021-04-14 13:20:48 +0200

    fakeDB + unix functions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,17 +4,15 @@
       src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
     ></v-parallax>
 
-      <v-container >
-        <h2 class="presentation" align="center" justify="center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-        </h2>
-      </v-container>
-
+    <v-container>
+      <h2 class="presentation" align="center" justify="center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </h2>
+    </v-container>
 
     <section class="blackBlock">
       <v-container>
@@ -26,7 +24,7 @@
             cols="12"
             sm="6"
             md="6"
-            lg="2"
+            lg="3"
           >
             <v-card class="mx-auto" max-width="400">
               <v-card-title>{{ menu.day }}</v-card-title>
@@ -36,13 +34,20 @@
               </v-card-subtitle>
 
               <v-card-text class="text--primary">
+                <h4>Plat principal</h4>
                 <div>{{ menu.meal }}</div>
+                <h4>Salades</h4>
+                <div v-for="(salad, index) in menus[index].salads" :key="index">
+                  {{ salad }}
+                </div>
+                <h4>Dessert</h4>
+                <div>{{ menu.dessert }}</div>
               </v-card-text>
             </v-card>
           </v-col>
         </v-row>
         <div style="text-align: center; padding: 40px 0px 40px 0px">
-          <v-btn elevation="2">Reserver</v-btn>
+          <v-btn elevation="2" @click="test()">Reserver</v-btn>
         </div>
       </v-container>
     </section>
@@ -50,9 +55,12 @@
 </template>
 
 <script>
+import fakeDB from "../JS/fakeDB";
+import dayjs from "dayjs";
+
 export default {
   name: "Home",
-
+  mixins: [fakeDB],
   data() {
     return {
       menus: [
@@ -60,34 +68,122 @@ export default {
           day: "Lundi",
           meal:
             "Escalope de dinde viennoise, Pommes de terre frite, Brocolis vapeur",
-          dessert: "dessert",
+          salads: [
+            "Salade grecque: icéberg, concombre cube, tomate cube, féta cube, oignons rouge, tomates cerises et olives",
+            "Salade Japonaise: Salade de choux pe-tsai (choux chinois), nouilles de riz, crevettes marinée au soja et wasabi, oranges, algues wakame, concombre, citron vert, graine de sesame",
+          ],
+          dessert: "Salade d'ananas",
         },
         {
           day: "Mardi",
           meal:
             "Escalope de dinde viennoise, Pommes de terre frite, Brocolis vapeur",
-          dessert: "dessert",
+
+          salads: [
+            "Salade grecque: icéberg, concombre cube, tomate cube, féta cube, oignons rouge, tomates cerises et olives",
+            "Salade Japonaise: Salade de choux pe-tsai (choux chinois), nouilles de riz, crevettes marinée au soja et wasabi, oranges, algues wakame, concombre, citron vert, graine de sesame",
+          ],
+          dessert: "Salade d'ananas",
         },
         {
           day: "Mercredi",
           meal:
             "Escalope de dinde viennoise, Pommes de terre frite, Brocolis vapeur",
-          dessert: "dessert",
+          salads: [
+            "Salade grecque: icéberg, concombre cube, tomate cube, féta cube, oignons rouge, tomates cerises et olives",
+            "Salade Japonaise: Salade de choux pe-tsai (choux chinois), nouilles de riz, crevettes marinée au soja et wasabi, oranges, algues wakame, concombre, citron vert, graine de sesame",
+          ],
+          dessert: "Salade d'ananas",
         },
         {
           day: "Jeudi",
           meal:
             "Escalope de dinde viennoise, Pommes de terre frite, Brocolis vapeur",
-          dessert: "dessert",
+          salads: [
+            "Salade grecque: icéberg, concombre cube, tomate cube, féta cube, oignons rouge, tomates cerises et olives",
+            "Salade Japonaise: Salade de choux pe-tsai (choux chinois), nouilles de riz, crevettes marinée au soja et wasabi, oranges, algues wakame, concombre, citron vert, graine de sesame",
+          ],
+          dessert: "Salade d'ananas",
         },
         {
           day: "Vendredi",
           meal:
             "Escalope de dinde viennoise, Pommes de terre frite, Brocolis vapeur",
-          dessert: "dessert",
+          salads: [
+            "Salade grecque: icéberg, concombre cube, tomate cube, féta cube, oignons rouge, tomates cerises et olives",
+            "Salade Japonaise: Salade de choux pe-tsai (choux chinois), nouilles de riz, crevettes marinée au soja et wasabi, oranges, algues wakame, concombre, citron vert, graine de sesame",
+          ],
+          dessert: "Salade d'ananas",
         },
       ],
     };
+  },
+
+  methods: {
+    test() {
+      console.log(this.data);
+    },
+
+    groupByDate(data) {
+      let map = new Map(Array.from(data, (obj) => [obj["deliveryDate"], []]));
+      data.forEach((obj) => map.get(obj["deliveryDate"]).push(obj));
+
+      return Array.from(map.values());
+    },
+
+    weekdayFromUnix(timeStamp) {
+      const weekday = new Array(7);
+      weekday[0] = "Dimanche";
+      weekday[1] = "Lundi";
+      weekday[2] = "Mardi";
+      weekday[3] = "Mercredi";
+      weekday[4] = "Jeudi";
+      weekday[5] = "Vendredi";
+      weekday[6] = "Samedi";
+
+      return weekday[dayjs.unix(timeStamp).$d.getDay()];
+    },
+
+    dateFromUnix(timeStamp) {
+      const month = new Array(7);
+      month[0] = "Janvier";
+      month[1] = "Février";
+      month[2] = "Mars";
+      month[3] = "Avril";
+      month[4] = "Mai";
+      month[5] = "Juin";
+      month[6] = "Juillet";
+      month[7] = "Août";
+      month[8] = "Septembre";
+      month[9] = "Octobre";
+      month[10] = "Novembre";
+      month[11] = "Décembre";
+
+      let date = dayjs.unix(timeStamp).$d;
+      let monthString = month[date.getUTCMonth()];
+      var day = date.getUTCDate();
+
+      return day + " " + monthString;
+    },
+  },
+
+  async mounted() {
+    try {
+      /*         const axios = require("axios");
+        let url = "http://localhost:8000/api/";
+        let result = await axios.get(url);  */
+
+      let data = this.plates;
+      data = this.groupByDate(data)
+
+let unique = [...new Set(data[0].map(item => item.description))];
+console.log(unique);
+      
+      console.log(this.weekdayFromUnix(1618322669));
+      console.log(this.dateFromUnix(1618322669));
+    } catch (e) {
+      console.log(e);
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
 import router from '@/plugins/router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
