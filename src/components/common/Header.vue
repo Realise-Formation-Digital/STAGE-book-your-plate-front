911de925fbc68a68491e48b8b474a5feabd113a2
Dario <dariogallego2420@gmail.com>
2021-04-23 16:03:45 +0200

    modified header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -13,6 +13,23 @@
         </router-link>
 
         <!-- buttons-------------------------------------------------------------------------------->
+        <div>
+          <nav class="navbar navbar-expand-lg fixed-top row">
+            
+              <router-link
+                  class="router"
+                  class-active="active"
+                  to="/ContactUs"
+                  exact
+                >
+                  <div class="btn">
+                    Feedback
+                  </div>
+              </router-link>
+              
+          </nav>
+        </div>
+        <!--  Button test
           <div class="container Feedback">
             <router-link to="/ContactUs">
               <button 
@@ -29,7 +46,7 @@
             </router-link>
 
           </div>
-        
+        -->
         <v-menu bottom left>
           <template v-slot:activator="{ on, attrs }">
             <!-- Account button -->
@@ -66,28 +83,20 @@
           v-model="group"
           active-class="dark--text text--accent-4"
         >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Test</v-list-item-title>
-          </v-list-item>
 
-          <v-list-item>
+          <v-list-item to="/ContactUs">
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
+            
             <v-list-item-title>
-              <v-btn
-                to="/ContactUs"
-                color="black"
-                class="ma-2 white--text"
-                @click="loader = 'loading3'"
-              >
-                <div class="router">Feedback</div>
-              </v-btn>
+                <div>
+                  <h1>Feedback</h1>
+                </div>
             </v-list-item-title>
+
           </v-list-item>
+          
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -111,9 +120,41 @@ export default {
 </script>
 
 <style scoped>
-.v-app-bar{
+/* nav Button */
+nav .btn:hover,
+nav .btn:active {
+  background-color: #009db2;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.router {
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+  font-size: 25px;
+  font-weight: 500;
+  
+}
+
+.router:hover{
+  color: white;
+}
+
+.btn {
+  margin-top: 3px;
+  padding: 15px 15px 15px 15px;
+  border-radius: 2%;
+}
+
+.FeedbackIn{
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+  background-color: #009db2;
+}
+
+.v-app-bar {
   padding-top: 10px;
-  padding-bottom: 75px
+  padding-bottom: 75px;
 }
 
 .NavContainer {
@@ -123,14 +164,12 @@ export default {
 }
 
 #logo {
-  margin-top: 38px;
   height: 40px;
   align-items: center;
   display: flex;
-  
 }
 
-.accountlogo {  
+.accountlogo {
   align-items: center;
   display: flex;
 }
@@ -139,12 +178,12 @@ export default {
   display: flex;
 }
 
-@media (max-width: 547px) {
-  .Feedback {
+@media (max-width: 768px) {
+  .btn {
     display: none;
   }
 
-  .test{
+  .test {
     display: none;
   }
 
@@ -152,73 +191,73 @@ export default {
     display: flex;
   }
 
-  #logo{
+  #logo {
     margin-top: 4px;
     height: 40px;
   }
 }
 
-@media (min-width: 547px) {
+@media (min-width: 769px) {
   .Hamburger {
     display: none;
   }
 }
 
-.Feedback{
-  text-align: center
+.Feedback {
+  text-align: center;
 }
 
 /* Buttons ----------------------------------------------------*/
 
-  @import url("https://fonts.googleapis.com/css?family=Raleway");
-  * {
-    box-sizing: border-box;
-  }
+@import url("https://fonts.googleapis.com/css?family=Raleway");
+* {
+  box-sizing: border-box;
+}
 
-  .button {
-    position: relative;
-    padding: 1em 1.5em;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    outline: none;
-    font-size: 18px;
-    margin: 1em 0.8em;
-  }
-  .button.type1 {
-    color: #000000;
-  }
-  .button.type1.type1::after,
-  .button.type1.type1::before {
-    content: "";
-    display: block;
-    position: absolute;
-    width: 30%;
-    height: 40%;
-    border: 2px solid;
-    transition: all 0.4s ease;
-    border-radius: 2px;
-  }
+.button {
+  position: relative;
+  padding: 1em 1.5em;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+  font-size: 18px;
+  margin: 1em 0.8em;
+}
+.button.type1 {
+  color: #000000;
+}
+.button.type1.type1::after,
+.button.type1.type1::before {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 30%;
+  height: 40%;
+  border: 2px solid;
+  transition: all 0.4s ease;
+  border-radius: 2px;
+}
 /* border colors */
-  .button.type1.type1::after {
-    bottom: 0;
-    right: 0;
-    border-top-color: transparent;
-    border-left-color: transparent;
-    border-bottom-color: #735656;
-    border-right-color: #566473;
-  }
-  .button.type1.type1::before {
-    top: 0;
-    left: 0;
-    border-bottom-color: transparent;
-    border-right-color: transparent;
-    border-top-color: #566473;
-    border-left-color: #566473;
-  }
-  .button.type1.type1:hover:after,
-  .button.type1.type1:hover:before {
-    width: 100%;
-    height: 100%;
-  }
+.button.type1.type1::after {
+  bottom: 0;
+  right: 0;
+  border-top-color: transparent;
+  border-left-color: transparent;
+  border-bottom-color: #735656;
+  border-right-color: #566473;
+}
+.button.type1.type1::before {
+  top: 0;
+  left: 0;
+  border-bottom-color: transparent;
+  border-right-color: transparent;
+  border-top-color: #566473;
+  border-left-color: #566473;
+}
+.button.type1.type1:hover:after,
+.button.type1.type1:hover:before {
+  width: 100%;
+  height: 100%;
+}
 </style>
